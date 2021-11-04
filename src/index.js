a8554b829ca437197056a9176e8401ebb6a6916e
Everton Christmann <evertonluiz00@hotmail.com>
2021-11-04 18:45:33 -0300

    Challenge done

--- src/index.js
+++ src/index.js
@@ -9,22 +9,80 @@ app.use(cors());
 
 const users = [];
 
+
+// Middlewares
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  let todosCountByUser = 0
+
+  user.todos.forEach(element => {
+    todosCountByUser++;
+  });
+  
+  if ((!user.pro) && todosCountByUser >= 10) {
+    return response.status(403).json({ error: 'Todos limit for this user!' });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id is not UUID!' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
+
+// Routes
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
